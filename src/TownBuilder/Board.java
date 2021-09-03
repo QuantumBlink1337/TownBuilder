c58bd6201ee62ebb792abcdae30e154bc1d967c4
QuantumBlink1337 <thequantumblink@gmail.com>
2021-09-02 21:14:38 -0500

    added user input options on their turn amended permutations of the Farm

--- src/TownBuilder/Board.java
+++ src/TownBuilder/Board.java
@@ -7,7 +7,7 @@ import TownBuilder.Buildings.RedBuilding;
 import java.util.Scanner;
 
 public class Board {
-    private final Player player;
+    //private final Player player;
     private TownResource[][] gameResourceBoard = new TownResource[4][4];
     private Building[][] gameBuildingBoard = new Building[4][4];
     private String[][] gameBoard = new String[4][4];
@@ -20,7 +20,7 @@ public class Board {
     private boolean redBuildingDetection;
 
     public Board() {
-        player = new Player();
+        //player = new Player();
         gameCompletion = false;
         buildArrays();
     }
@@ -42,17 +42,16 @@ public class Board {
         for (int row = 0; row < coordinateBoard.length; row++) {
             for (int col = 0; col < coordinateBoard[row].length; col++) {
                 coordinateBoard[row][col] = "[Row: "+row+" Col: "+col+"]";
-
             }
         }
-
-
     }
     public void game() {
         while (!gameCompletion) {
             renderBoard();
             playerTurn();
+            System.out.println("Loop broke");
             detection();
+
 
         }
     }
@@ -66,14 +65,27 @@ public class Board {
     }
     public void playerTurn() {
         String playerChoice = "";
-        System.out.println("What would you like to do this turn?");
-        playerChoice = sc.nextLine().toLowerCase().strip();
-        if (playerChoice.equals("clear")) {
-            buildArrays();
+        boolean validInput = false;
+        do {
+            System.out.println("What would you like to do this turn?");
+            playerChoice = sc.nextLine().toLowerCase().strip();
+            System.out.print(playerChoice);
+            if (playerChoice.equals("clear")) {
+                buildArrays();
+                validInput = true;
+            } else if (playerChoice.equals("place")) {
+                ResourceEnum turnResource = ResourceEnum.randomResource();
+                System.out.println("Your resource for this turn is " + turnResource);
+                validInput = true;
+                resourcePlacer(turnResource);
+
+            } else if (playerChoice.equals("construct")) {
+            } else {
+                System.out.println("Not a valid action");
+                validInput = true;
+            }
         }
-        ResourceEnum turnResource = ResourceEnum.randomResource();
-        System.out.println("Your resource for this turn is "+turnResource);
-        resourcePlacer(turnResource);
+        while (!validInput);
     }
     public void resourcePlacer(ResourceEnum random) {
         String userCoordinate = "   ";
--- src/TownBuilder/Buildings/RedBuilding.java
+++ src/TownBuilder/Buildings/RedBuilding.java
@@ -17,46 +17,101 @@ public class RedBuilding extends Building {
     }
     public static boolean redDetection(int row, int col, TownResource[][] resourceArray) {
         System.out.println("Row: " + row + " Col: " + col);
-        if (resourceArray[row][col].getResource() == ResourceEnum.WHEAT && !resourceArray[row][col].getScannedStatus()) {
-            System.out.println("Wheat found. Beginning scan sequence.");
-            System.out.println("Modded Row: " + (row)+ " Col: " + col);
-            if (resourceArray[row][col+1].getResource() == ResourceEnum.WHEAT) {
-                System.out.println("Row: " + row + " Modded Col: " + (col+1));
-                System.out.println("Wheat found. 2/4 found");
-                if (resourceArray[row+1][col].getResource() == ResourceEnum.WOOD) {
-                    System.out.println("Wood found. 3/4 found");
-                    System.out.println("Modded Row: " + (row) + " Modded Col: " + (col+1));
-                    if (resourceArray[row+1][col+1].getResource() == ResourceEnum.WOOD) {
-                        System.out.println("Modded Row: " + (row+1) + " Modded Col: " + (col+1));
-                        resourceArray[row][col].setScannedStatus();
-                        resourceArray[row+1][col].setScannedStatus();
-                        resourceArray[row][col+1].setScannedStatus();
-                        resourceArray[row+1][col+1].setScannedStatus();
-                        System.out.println("FARM FOUND!");
-                        return true;
+        try {
+            if (resourceArray[row][col].getResource() == ResourceEnum.WHEAT && !resourceArray[row][col].getScannedStatus()) {
+                System.out.println("Wheat found. Beginning scan sequence.");
+                System.out.println("Modded Row: " + (row) + " Col: " + col);
+                if (resourceArray[row][col + 1].getResource() == ResourceEnum.WHEAT) {
+                    System.out.println("Row: " + row + " Modded Col: " + (col + 1));
+                    System.out.println("Wheat found. 2/4 found");
+                    if (resourceArray[row - 1][col].getResource() == ResourceEnum.WOOD)
+                    {
+                        System.out.println("Wood found. 3/4 found");
+                        System.out.println("Modded Row: " + (row) + " Modded Col: " + (col + 1));
+                        if (resourceArray[row - 1][col - 1].getResource() == ResourceEnum.WOOD) {
+                            System.out.println("Modded Row: " + (row + 1) + " Modded Col: " + (col + 1));
+                            resourceArray[row][col].setScannedStatus();
+                            resourceArray[row + 1][col].setScannedStatus();
+                            resourceArray[row][col + 1].setScannedStatus();
+                            resourceArray[row + 1][col + 1].setScannedStatus();
+                            System.out.println("FARM FOUND!");
+                            return true;
+                        }
                     }
                 }
             }
-            if (resourceArray[row][col+1].getResource() == ResourceEnum.WHEAT) {
+        }
+            catch (Exception e) {
+            }
+            try {
+                if (resourceArray[row][col+1].getResource() == ResourceEnum.WHEAT) {
+                    System.out.println("Row: " + row + " Modded Col: " + (col+1));
+                    System.out.println("Wheat found. 2/4 found");
+                    if (resourceArray[row-1][col].getResource() == ResourceEnum.WOOD) {
+                        System.out.println("Wood found. 3/4 found");
+                        System.out.println("Modded Row: " + (row) + " Modded Col: " + (col+1));
+                        if (resourceArray[row-1][col+1].getResource() == ResourceEnum.WOOD) {
+                            System.out.println("Modded Row: " + (row+1) + " Modded Col: " + (col+1));
+                            resourceArray[row][col].setScannedStatus();
+                            resourceArray[row][col+1].setScannedStatus();
+                            resourceArray[row-1][col].setScannedStatus();
+                            resourceArray[row-1][col+1].setScannedStatus();
+                            System.out.println("FARM FOUND!");
+                            return true;
+                        }
+                    }
+                }
+            }
+            catch (Exception e) {
+            }
+            try {
+                if (resourceArray[row+1][col].getResource() == ResourceEnum.WHEAT) {
+                    System.out.println("Row: " + row + " Modded Col: " + (col+1));
+                    System.out.println("Wheat found. 2/4 found");
+                    if (resourceArray[row][col+1].getResource() == ResourceEnum.WOOD) {
+                        System.out.println("Wood found. 3/4 found");
+                        System.out.println("Modded Row: " + (row) + " Modded Col: " + (col+1));
+                        if (resourceArray[row-1][col+1].getResource() == ResourceEnum.WOOD) {
+                            System.out.println("Modded Row: " + (row+1) + " Modded Col: " + (col+1));
+                            resourceArray[row][col].setScannedStatus();
+                            resourceArray[row][col+1].setScannedStatus();
+                            resourceArray[row-1][col].setScannedStatus();
+                            resourceArray[row-1][col+1].setScannedStatus();
+                            System.out.println("FARM FOUND!");
+                            return true;
+                        }
+                    }
+                }
+            }
+            catch(Exception e) {
+            }
+        try {
+            if (resourceArray[row+1][col].getResource() == ResourceEnum.WHEAT) {
                 System.out.println("Row: " + row + " Modded Col: " + (col+1));
                 System.out.println("Wheat found. 2/4 found");
-                if (resourceArray[row+1][col].getResource() == ResourceEnum.WOOD) {
+                if (resourceArray[row][col-1].getResource() == ResourceEnum.WOOD) {
                     System.out.println("Wood found. 3/4 found");
                     System.out.println("Modded Row: " + (row) + " Modded Col: " + (col+1));
-                    if (resourceArray[row+1][col+1].getResource() == ResourceEnum.WOOD) {
+                    if (resourceArray[row+1][col-1].getResource() == ResourceEnum.WOOD) {
                         System.out.println("Modded Row: " + (row+1) + " Modded Col: " + (col+1));
                         resourceArray[row][col].setScannedStatus();
-                        resourceArray[row+1][col].setScannedStatus();
                         resourceArray[row][col+1].setScannedStatus();
-                        resourceArray[row+1][col+1].setScannedStatus();
+                        resourceArray[row-1][col].setScannedStatus();
+                        resourceArray[row-1][col+1].setScannedStatus();
                         System.out.println("FARM FOUND!");
                         return true;
                     }
                 }
             }
         }
-
+        catch(Exception e) {
+        }
 
         return false;
-     }
+        }
+
+
+
+
+
 }
