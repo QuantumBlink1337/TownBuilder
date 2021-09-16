a7c5fae16834790d9bc2fd43c5009c221b899348
Matt Marlow <thequantumblink@gmail.com>
2021-09-16 18:03:29 -0500

    general cleanup, commented out debug prints

--- src/TownBuilder/Buildings/Building.java
+++ src/TownBuilder/Buildings/Building.java
@@ -78,24 +78,22 @@ public class Building {
     }
     public static boolean detection(int row, int col, TownResource[][] rArray, ResourceEnum[][][] bT, BuildingEnum buildingType) {
             try {
-                System.out.println("Received detection call for " + buildingType);
-                System.out.println("Length of bT: " + bT.length);
+                //System.out.println("Received detection call for " + buildingType);
+                //System.out.println("Length of bT: " + bT.length);
                 int checkTime = 0;
                 int patternIndex = 0;
                 for (ResourceEnum[][] board : bT) {
                     checkTime += 4;
                 }
-                System.out.println("CheckTime: " + checkTime);
-
-
+                //System.out.println("CheckTime: " + checkTime);
                 ResourceEnum[][][] buildingTemplate = bT;
                 for (int i = 1; i < checkTime+1; i++) {
-                    System.out.println("i: " + i);
+                    //System.out.println("i: " + i);
                     if (i % 5 == 0) {
-                        System.out.println("Switching to next pattern available");
+                        //System.out.println("Switching to next pattern available");
                         patternIndex++;
                     }
-                    Utility.arrayPrinter(bT[patternIndex]);
+                    //Utility.arrayPrinter(bT[patternIndex]);
                     if (compare(row, col, rArray, buildingTemplate[patternIndex], buildingType)) {
                         for (int j = 0; j < validResources.size(); j++) {
                             validResources.get(j).setScannedBuilding(buildingType);
@@ -108,44 +106,40 @@ public class Building {
                 }
             }
             catch (ArrayIndexOutOfBoundsException e){
-                System.out.println("Out of bounds exception?");
+                //System.out.println("Out of bounds exception?");
             }
-
         return false;
     }
-    public static boolean compare(int row, int col, TownResource[][] rArray, ResourceEnum[][] buildingTemplate, BuildingEnum buildingType) {
+    private static boolean compare(int row, int col, TownResource[][] rArray, ResourceEnum[][] buildingTemplate, BuildingEnum buildingType) {
         for (int r = 0; r < buildingTemplate.length; r++) {
             for (int c = 0; c < buildingTemplate[r].length; c++) {
                 if (match(rArray[row+r][col+c], buildingTemplate[r][c])) {
-                    System.out.println("Successful comparison at " + (row+r) + ", " + (col+c));
+                    //System.out.println("Successful comparison at " + (row+r) + ", " + (col+c));
                 }
                 else {
-                    System.out.println("Failure of comparison at " + (row+r) + ", " + (col+c));
+                    //System.out.println("Failure of comparison at " + (row+r) + ", " + (col+c));
                     validResources.clear();
                     return false;
                 }
             }
         }
         return true;
-
     }
-
-    public static boolean match(TownResource toBeChecked, ResourceEnum checker) {
+    private static boolean match(TownResource toBeChecked, ResourceEnum checker) {
         if (checker == ResourceEnum.NONE) {
-            System.out.println("Checked tile was matched to NONE, therefore it is true");
+            //System.out.println("Checked tile was matched to NONE, therefore it is true");
             return true;
         }
         else if (toBeChecked.getResource() == checker) {
             validResources.add(toBeChecked);
-            System.out.println("Checked tile was matched to an arbitrary value " + checker + ", therefore it is true");
+            //System.out.println("Checked tile was matched to an arbitrary value " + checker + ", therefore it is true");
             return true;
         }
-        System.out.println("Checked tile has no match");
+        //System.out.println("Checked tile has no match");
         return false;
     }
-
-    public static ResourceEnum[][] buildingRotation(ResourceEnum[][] a) {
-        System.out.println("No match found. Flipping array...");
+    private static ResourceEnum[][] buildingRotation(ResourceEnum[][] a) {
+        //System.out.println("No match found. Flipping array...");
         final int M = a.length;
         final int N = a[0].length;
         ResourceEnum[][] ret = new ResourceEnum[N][M];
@@ -154,9 +148,9 @@ public class Building {
                 ret[c][M - 1 - r] = a[r][c];
             }
         }
-        System.out.println("Flipped array:");
-        System.out.println("["+ret[0][0]+"]["+ret[0][1]+"]");
-        System.out.println("["+ret[1][0]+"]["+ret[1][1]+"]");
+        //System.out.println("Flipped array:");
+        //System.out.println("["+ret[0][0]+"]["+ret[0][1]+"]");
+        //System.out.println("["+ret[1][0]+"]["+ret[1][1]+"]");
         return ret;
     }
 }
