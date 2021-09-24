2b382d9c3f3fff662389094e72b8d34c2ecd1a4c
Matt Marlow <thequantumblink@gmail.com>
2021-09-24 12:40:39 -0500

    manual class added to help faciliate player help

---
+++ src/TownBuilder/Manual.java
@@ -0,0 +1,12 @@
+package TownBuilder;
+
+import TownBuilder.Buildings.Building;
+
+public class Manual {
+        private Building[] gameBuildings;
+    public Manual(Building[] b) {
+        gameBuildings = b;
+        System.out.println("New manual created.");
+    }
+
+}
