cf14557a7fdc8e63a7baf22bbab6efa083ba3544
Matt Marlow <thequantumblink@gmail.com>
2021-09-24 13:10:24 -0500

    arrayPrinter can handle arbitrary sizes now

--- src/TownBuilder/Utility.java
+++ src/TownBuilder/Utility.java
@@ -11,21 +11,11 @@ public class Utility {
     public static <T> void arrayPrinter(T[][] array) {
         for (int row = 0; row < array.length; row++) {
             for (int col = 0; col < array[row].length; col++) {
-                if (array.length == 4) {
-                    if (col == 3) {
-                        System.out.println(array[row][col]);
-                    }
-                    else {
-                        System.out.print(array[row][col]);
-                    }
+                if (col == (array[row].length - 1)) {
+                    System.out.println("[" + array[row][col] + "]");
                 }
-                else if (array.length == 2) {
-                    if (col == 1) {
-                        System.out.println(array[row][col]);
-                    }
-                    else {
-                        System.out.print(array[row][col]);
-                    }
+                else {
+                    System.out.print("[" + array[row][col] + "]");
                 }
             }
         }
