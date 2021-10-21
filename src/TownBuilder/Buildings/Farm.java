72cc3a64baa92ae7dc89d70ea34f107515a7f099
Matt Marlow <thequantumblink@gmail.com>
2021-10-20 20:03:32 -0500

    farm uses new onTurnInterval method to dynamically "Feed" other buildings instead of at scoretime

--- src/TownBuilder/Buildings/Farm.java
+++ src/TownBuilder/Buildings/Farm.java
@@ -4,9 +4,9 @@ import TownBuilder.ResourceEnum;
 import TownBuilder.Utility;
 
 import java.util.ArrayList;
-
-public class Farm extends Building {
-    private final boolean condition;
+import static TownBuilder.Buildings.BuildingFactory.patternBuilder;
+public class Farm implements Building {
+    private boolean condition;
     private int fed;
     private static final ResourceEnum[][] farmArray = new ResourceEnum[2][2];
     private static final ArrayList<ResourceEnum[][]> farmPatternList = new ArrayList<>();
@@ -28,6 +28,12 @@ public class Farm extends Building {
     public boolean getCondition() {
         return condition;
     }
+
+    @Override
+    public void setCondition(boolean condition) {
+        this.condition = condition;
+    }
+
     public void decrementFed() {
         --fed;
     }
@@ -37,17 +43,35 @@ public class Farm extends Building {
     public String toString() {
         return "Farm";
     }
+
+    @Override
+    public boolean isFeedable() {
+        return false;
+    }
+
     public void printManualText() {
         System.out.println("The Farm feeds up to four buildings on the board.");
         System.out.println("Here's what it looks like:");
         Utility.arrayPrinter(farmArray);
     }
-    public ArrayList<ResourceEnum[][]> getPatterns() {
+    public ArrayList<ResourceEnum[][]> getBuildingPatternsList() {
 
         return farmPatternList;
     }
     public int scorer(Building[][] bArray, int row, int col, int scoreIncrement) {
         return 0;
+    }
+
+    @Override
+    public void onTurnInterval(Building[][] buildingBoard, int row, int col) {
+        for (Building[] buildingRow : buildingBoard) {
+            for (Building building : buildingRow) {
+                if (building.isFeedable() && !building.getCondition() && fed > 0) {
+                    building.setCondition(true);
+                    fed--;
+                }
+            }
+        }
     }
 
 }
