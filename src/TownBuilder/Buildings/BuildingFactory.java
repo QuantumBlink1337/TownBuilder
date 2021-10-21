62e1d35446e4367e85c37c816a4466b78444a8cf
Matt Marlow <thequantumblink@gmail.com>
2021-10-20 20:00:02 -0500

    BuildingFactory builds master hashmap

--- src/TownBuilder/Buildings/BuildingFactory.java
+++ src/TownBuilder/Buildings/BuildingFactory.java
@@ -5,12 +5,25 @@ import TownBuilder.ResourceEnum;
 import TownBuilder.Utility;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class BuildingFactory {
     private final ArrayList<Resource> validResources = new ArrayList<>();
+    private static final HashMap<String, ArrayList<Building>> buildingMasterList = new HashMap<>();
+    public static void setbuildingMasterList() {
 
-    public Building getBuilding(BuildingEnum buildingEnum) {
+        buildingMasterList.put("blue", new ArrayList<>(Arrays.asList(new Cottage())));
+        buildingMasterList.put("red", new ArrayList<>(Arrays.asList(new Farm())));
+        buildingMasterList.put("gray", new ArrayList<>(Arrays.asList(new Well())));
+        buildingMasterList.put("orange", new ArrayList<>(Arrays.asList(new Chapel())));
+        buildingMasterList.put("green", new ArrayList<>(Arrays.asList(new Tavern())));
+        buildingMasterList.put("yellow", new ArrayList<>(Arrays.asList(new Theater())));
+        buildingMasterList.put("black", new ArrayList<>(Arrays.asList(new Warehouse())));
+    }
+
+    public Building getBuilding(BuildingEnum buildingEnum, ArrayList<Building> buildingMasterList) {
         if (buildingEnum == BuildingEnum.CHAPEL) {
             return new Chapel();
         }
@@ -24,7 +37,7 @@ public class BuildingFactory {
             return new Tavern();
         }
         else if (buildingEnum == BuildingEnum.THEATER) {
-            return new Theater();
+            return new Theater(buildingMasterList);
         }
         else if (buildingEnum == BuildingEnum.WHOUSE) {
             return new Warehouse();
@@ -37,6 +50,9 @@ public class BuildingFactory {
     public ArrayList<Resource> getValidResources() {
         return validResources;
     }
+    public static HashMap<String, ArrayList<Building>> getBuildingMasterList() {
+        return buildingMasterList;
+    }
 
     public void clearResources(BuildingEnum building) {
         for (int i = 0; i < validResources.size(); i++) {
@@ -44,6 +60,24 @@ public class BuildingFactory {
                 validResources.remove(i);
             }
         }
+    }
+    public static void patternBuilder(ResourceEnum[][] pattern, ArrayList<ResourceEnum[][]> patternList, int rotations) {
+        if (rotations >= 0) {
+            ResourceEnum[][] rotatedPattern = buildingRotation(pattern);
+            patternList.add(rotatedPattern);
+            patternBuilder(rotatedPattern, patternList, rotations-1);
+        }
+    }
+    protected static ResourceEnum[][] buildingRotation(ResourceEnum[][] a) {
+        final int M = a.length;
+        final int N = a[0].length;
+        ResourceEnum[][] ret = new ResourceEnum[N][M];
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                ret[c][M - 1 - r] = a[r][c];
+            }
+        }
+        return ret;
     }
     public boolean detection(int row, int col, Resource[][] rArray, ArrayList<ResourceEnum[][]> bT, BuildingEnum buildingType) {
         for (ResourceEnum[][] resourceEnums : bT) {
@@ -83,12 +117,12 @@ public class BuildingFactory {
         }
         return false;
     }
-        public void placement(Resource[][] rArray, Building[][] bArray, BuildingEnum buildingEnum) throws InstantiationException, IllegalAccessException {
+        public void placement(Resource[][] rArray, Building[][] bArray, BuildingEnum buildingEnum, ArrayList<Building> buildingArrayList) throws InstantiationException, IllegalAccessException {
         Scanner sc = new Scanner((System.in));
         String userInput;
         int[] coords;
         boolean validInput = false;
-        Building building = getBuilding(buildingEnum);
+        Building building = getBuilding(buildingEnum,buildingArrayList);
         do {
             System.out.println("Where would you like to place your " + building.toString() + "?");
             System.out.println("Valid positions for the "+building.toString()+ " are:");
