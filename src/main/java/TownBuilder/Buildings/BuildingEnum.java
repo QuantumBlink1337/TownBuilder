d28a0eaa0bd6f799cffc3306432c005164207095
Matt Marlow <thequantumblink@gmail.com>
2021-10-24 14:17:06 -0500

    BuildingEnum now is related to new ColorEnum

--- src/main/java/TownBuilder/Buildings/BuildingEnum.java
+++ src/main/java/TownBuilder/Buildings/BuildingEnum.java
@@ -1,24 +1,32 @@
 package TownBuilder.Buildings;
 
 public enum BuildingEnum {
-    FARM,
-    GRANARY,
-    GRENHOUSE,
-    ORCHARD,
-    CHAPEL,
-    ABBEY,
-    CLOISTER,
-    TEMPLE,
-    COTTAGE,
-    THEATER,
-    BAKERY,
-    TAVERN,
-    ALMSHOUSE,
-    FEASTHALL,
-    WELL,
-    FOUNTAIN,
-    MILLSTONE,
-    WAREHOUSE,
-    NONE;
+    FARM (ColorEnum.RED),
+    GRANARY (ColorEnum.RED),
+    GRENHOUSE (ColorEnum.RED),
+    ORCHARD (ColorEnum.RED),
+    CHAPEL (ColorEnum.ORANGE),
+    ABBEY (ColorEnum.ORANGE),
+    CLOISTER (ColorEnum.ORANGE),
+    TEMPLE (ColorEnum.ORANGE),
+    COTTAGE (ColorEnum.BLUE),
+    THEATER (ColorEnum.YELLOW),
+    BAKERY (ColorEnum.YELLOW),
+    TAVERN (ColorEnum.GREEN),
+    ALMSHOUSE (ColorEnum.GREEN),
+    FEASTHALL (ColorEnum.GREEN),
+    WELL (ColorEnum.GRAY),
+    FOUNTAIN (ColorEnum.GRAY),
+    MILLSTONE (ColorEnum.GRAY),
+    WAREHOUSE (ColorEnum.BLACK),
+    NONE (ColorEnum.COLORLESS);
 
+    private final ColorEnum color;
+    BuildingEnum(ColorEnum color) {
+        this.color = color;
     }
+
+    public ColorEnum getColor() {
+        return color;
+    }
+}
---
+++ src/main/java/TownBuilder/Buildings/ColorEnum.java
@@ -0,0 +1,12 @@
+package TownBuilder.Buildings;
+
+public enum ColorEnum {
+    RED,
+    YELLOW,
+    BLUE,
+    GREEN,
+    GRAY,
+    ORANGE,
+    BLACK,
+    COLORLESS;
+}
