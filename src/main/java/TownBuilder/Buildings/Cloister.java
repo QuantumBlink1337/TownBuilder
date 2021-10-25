a2c4085351dac0d7d41aec1aff092e467953b333
Matt Marlow <thequantumblink@gmail.com>
2021-10-25 09:43:25 -0500

    fixed bug in Cloister where any building would count for corner point

--- src/main/java/TownBuilder/Buildings/Cloister.java
+++ src/main/java/TownBuilder/Buildings/Cloister.java
@@ -70,8 +70,10 @@ public class Cloister implements Building {
             for (Building building : buildingRow) {
                 int r = building.getRow();
                 int c = building.getCol();
-                if ((r == 0 && c == 0) || (r == 0 && c == bArray[r].length -1) || (r == bArray.length -1 && c == 0) || (r == bArray.length -1 && c == bArray[r].length -1)) {
-                    score+=1;
+                if (building.getType() == BuildingEnum.CLOISTER) {
+                    if ((r == 0 && c == 0) || (r == 0 && c == bArray[r].length -1) || (r == bArray.length -1 && c == 0) || (r == bArray.length -1 && c == bArray[r].length -1)) {
+                        score+=1;
+                    }
                 }
             }
         }
