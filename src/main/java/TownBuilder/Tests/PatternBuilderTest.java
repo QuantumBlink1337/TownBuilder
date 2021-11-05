748d7235e59a63a21c649bf41ed1752bd28f603c
DownrightNifty <51222679+DownrightNifty@users.noreply.github.com>
2021-11-05 13:59:44 -0500

    Check for horizontal symmetry as well
    Previously, we only checked for vertical symmetry and not horizontal symmetry, which meant we unnecessarily computed mirror transformations sometimes. Now we check for both, saving a bit of processing power.

---
+++ src/main/java/TownBuilder/Tests/PatternBuilderTest.java
@@ -0,0 +1,33 @@
+package TownBuilder.Tests;
+
+import java.util.ArrayList;
+
+import TownBuilder.Buildings.*;
+
+public class PatternBuilderTest {
+    public static void main(String[] args) {
+        // vertically symmetric
+        // total should be 4: pattern definition (1) and its rotations (3)
+        ArrayList<ResourceEnum[][]> b1 = (new Theater()).getBuildingPatternsList();
+        System.out.println("[ All transformations of Theater ]\n");
+        Utility.printMembersof3dArrayList(b1);
+        assert b1.size() == 4;
+        System.out.println("----\n");
+
+        // horizontally symmetric
+        // total should be 4: pattern definition (1) and its rotations (3)
+        ArrayList<ResourceEnum[][]> b2 = (new Tavern(0, 0)).getBuildingPatternsList();
+        System.out.println("[ All transformations of Tavern ]\n");
+        Utility.printMembersof3dArrayList(b2);
+        assert b2.size() == 4;
+        System.out.println("----\n");
+
+        // non-symmetrical
+        // total should be 8: pattern definition (1) and its rotations (3), and mirror (1) and its rotations (3)
+        ArrayList<ResourceEnum[][]> b3 = (new Chapel(0, 0)).getBuildingPatternsList();
+        System.out.println("[ All transformations of Chapel ]\n");
+        Utility.printMembersof3dArrayList(b3);
+        assert b3.size() == 8;
+        System.out.println("----\n");
+    }
+}
