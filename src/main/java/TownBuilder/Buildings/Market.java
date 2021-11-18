b3dcac9e7a98c5015ef487466ec3a127fe53a735
Matt Marlow <thequantumblink@gmail.com>
2021-11-18 13:45:17 -0600

    manual text is in correct zone

--- src/main/java/TownBuilder/Buildings/Market.java
+++ src/main/java/TownBuilder/Buildings/Market.java
@@ -79,14 +79,14 @@ public class Market implements Building {
 
     @Override
     public void onTurnInterval(Building[][] buildingBoard) {
-        System.out.println("The Market earns points for each other Yellow building in a row or column, whichever has more.");
-        System.out.println("For example, if you have 2 Yellow buildings in the same row as your original, but 3 in the Column, then your\nbuilding is worth 3 points.");
-        System.out.println("Here's what it looks like:");
-        Utility.arrayPrinter(marketArray);
+
     }
 
     @Override
     public void printManualText() {
-
+        System.out.println("The Market earns points for each other Yellow building in a row or column, whichever has more.");
+        System.out.println("For example, if you have 2 Yellow buildings in the same row as your original, but 3 in the Column, then your\nbuilding is worth 3 points.");
+        System.out.println("Here's what it looks like:");
+        Utility.arrayPrinter(marketArray);
     }
 }
