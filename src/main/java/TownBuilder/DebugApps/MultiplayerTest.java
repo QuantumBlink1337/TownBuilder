2739ddf2e6af063ef95280f360be08e39021ce6c
Matt Marlow <thequantumblink@gmail.com>
2021-12-02 15:52:07 -0600

    new MultiplayerTest option

---
+++ src/main/java/TownBuilder/DebugApps/MultiplayerTest.java
@@ -0,0 +1,36 @@
+package TownBuilder.DebugApps;
+
+import TownBuilder.*;
+import TownBuilder.Buildings.Monuments.OpaleyeWatch;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static TownBuilder.DebugApps.DebugTools.*;
+
+public class MultiplayerTest {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        logging("THIS IS NOT THE ACTUAL GAME. THIS IS A TEST OF THE MULTIPLAYER SYSTEM.");
+        Utility.setColor(true);
+        GameInitializer gameInitializer = new GameInitializer();
+        gameInitializer.buildingSelection();
+        PlayerManager playerManager = new PlayerManager(gameInitializer.getBuildingsForGame());
+        playerManager.determineNumberOfBoards();
+        Board OpaleyeBoard = null;
+        for (Board board : playerManager.getBoards()) {
+            if (board.getScorableBuildings().get(board.getScorableBuildings().size() - 1) instanceof OpaleyeWatch) {
+                OpaleyeBoard = board;
+            }
+        }
+        Resource[][] rBoard = OpaleyeBoard.getGameResourceBoard();
+        rBoard[0][0] = new Resource(ResourceEnum.WOOD, 0, 0);
+        rBoard[1][0] = new Resource(ResourceEnum.BRICK, 1, 0);
+        rBoard[1][1] = new Resource(ResourceEnum.GLASS, 1, 1);
+        rBoard[1][2] = new Resource(ResourceEnum.WHEAT, 1, 2);
+        rBoard[1][3] = new Resource(ResourceEnum.WHEAT, 1, 3);
+        do {
+            playerManager.manageTurn();
+        }
+        while (playerManager.gameActive());
+    }
+}
