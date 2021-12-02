8bad5fcd9b6de2402204c108d0d5c768af39e99a
Matt Marlow <thequantumblink@gmail.com>
2021-12-02 15:00:03 -0600

    implemented Opaleye logic

--- src/main/java/TownBuilder/Buildings/Monuments/OpaleyeWatch.java
+++ src/main/java/TownBuilder/Buildings/Monuments/OpaleyeWatch.java
@@ -5,6 +5,7 @@ import TownBuilder.Buildings.Building;
 import TownBuilder.Buildings.BuildingEnum;
 import TownBuilder.Buildings.BuildingFactory;
 import TownBuilder.ResourceEnum;
+import TownBuilder.Utility;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class OpaleyeWatch implements Monument{
     private final int col;
     private final Board board;
     private boolean condition;
+    private final ArrayList<BuildingEnum> chosenBuildings = new ArrayList<>();
     static {
         pattern[0] = new ResourceEnum[]{ResourceEnum.WOOD, ResourceEnum.NONE,ResourceEnum.NONE,ResourceEnum.NONE};
         pattern[1] = new ResourceEnum[]{ResourceEnum.BRICK,ResourceEnum.GLASS,ResourceEnum.WHEAT,ResourceEnum.WHEAT};
@@ -65,26 +67,38 @@ public class OpaleyeWatch implements Monument{
 
     @Override
     public void onTurnInterval(Building[][] buildingBoard) throws IOException {
-
+        Board[] adjacentBoards = board.getPlayerManager().getAdjacentBoards(board);
+        for (Board board : adjacentBoards) {
+            if (board.getLastBuiltBuilding() == chosenBuildings.get(0) || board.getLastBuiltBuilding() == chosenBuildings.get(1) ||board.getLastBuiltBuilding() == chosenBuildings.get(2)) {
+                board.getBuildingFactory().placeBuildingOnBoard(board.getLastBuiltBuilding(), board.getDetectableBuildings(), true, board);
+                chosenBuildings.remove(board.getLastBuiltBuilding());
+                break;
+            }
+        }
     }
 
     @Override
     public void printManualText() {
-
+        System.out.println("Upon placement, declare three different buildings.\nWhenever a player to your left or right constructs one of those buildings,\nplace that building on an empty spot on your board\nand remove it from Opaleye's Watch.");
+        System.out.println("Here's what it looks like:");
+        Utility.arrayPrinter(pattern);
     }
 
     @Override
     public void onPlacement() throws IOException {
-
+        System.out.println("Choose three buildings to track.");
+        for (int i = 0; i < 3; i++) {
+            chosenBuildings.set(i, board.buildingPlacer(board.getScorableBuildings(), false));
+        }
     }
 
     @Override
     public int getRow() {
-        return 0;
+        return row;
     }
 
     @Override
     public int getCol() {
-        return 0;
+        return col;
     }
 }
