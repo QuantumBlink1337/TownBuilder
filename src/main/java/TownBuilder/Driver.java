c2d178b15606ca0d53dc1cadda2e729f8be7bf03
Matt Marlow <thequantumblink@gmail.com>
2021-12-03 22:33:22 -0600

    removed redundant method

--- src/main/java/TownBuilder/Driver.java
+++ src/main/java/TownBuilder/Driver.java
@@ -20,8 +20,4 @@ public class Driver {
         while(playerManager.gameActive());
         System.out.println("All players have finished TownBuilder. Thanks for playing! -Matt");
     }
-    private static void colorPrintingDetermination() {
-        System.out.println("Are you playing on a Windows terminal? (Powershell or Command Line)");
-        Utility.setColor(!Utility.prompt());
-    }
 }
