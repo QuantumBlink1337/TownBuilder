1873fc2a5058305500751785135c7af17b625c50
Matt Marlow <thequantumblink@gmail.com>
2021-12-04 15:34:04 -0600

    testing file

---
+++ src/main/java/TownBuilder/DebugApps/UITest.java
@@ -0,0 +1,15 @@
+package TownBuilder.DebugApps;
+
+import TownBuilder.UI.BoardUILayer;
+
+
+public class UITest {
+    public static void main(String[] args) {
+        BoardUILayer layer = new BoardUILayer();
+        layer.setVisible(true);
+//        TileUI buttonTest = new TileUI(1, 2);
+//        buttonTest.setBounds(50, 50, 100, 100);
+//        layer.add(buttonTest);
+        layer.updateBoardTiles();
+    }
+}
