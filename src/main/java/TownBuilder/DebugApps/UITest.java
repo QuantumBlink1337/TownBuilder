5d8a182c0b309f523dff5d1c6be9567b0055291e
Matt Marlow <thequantumblink@gmail.com>
2022-01-07 17:40:23 -0600

    removed resource called from player turn

--- src/main/java/TownBuilder/DebugApps/UITest.java
+++ src/main/java/TownBuilder/DebugApps/UITest.java
@@ -2,9 +2,6 @@ package TownBuilder.DebugApps;
 
 import TownBuilder.Board;
 import TownBuilder.Buildings.*;
-import TownBuilder.Buildings.Monuments.Starloom;
-import TownBuilder.UI.BoardUI;
-import TownBuilder.Utility;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -17,11 +14,13 @@ public class UITest {
         ArrayList<Building> masterBuildings = new ArrayList<>(Arrays.asList(new Cottage(-1, -1), new Granary(-1, -1), new Shed(-1, -1), new Abbey(-1, -1), new Bakery(-1, -1), new Almshouse(-1, -1), new Warehouse(-1, -1)));
 
         Board board = new Board(masterBuildings, BuildingEnum.STARLOOM, null);
-
+        board.getGameBuildingBoard()[3][3] = BuildingFactory.getBuilding(BuildingEnum.WAREHOUSE, masterBuildings, 3, 3, false);
+        board.setLastBuiltBuilding(BuildingEnum.WAREHOUSE);
         while (!board.isGameCompletion()) {
-            board.playerTurn(board.resourcePicker());
+            board.playerTurn();
             board.detectValidBuilding();
             board.setGameCompletion(board.gameOver());
+            board.getBoardUI().repaint();
         }
 
 
