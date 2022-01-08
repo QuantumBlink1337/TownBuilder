03372dcad78fcb4f91740ffeb6723665c2d20d39
Matt Marlow <thequantumblink@gmail.com>
2022-01-08 16:03:28 -0600

    moved resource methods to Utility (to do: move to dedicated resourceUtility class)

--- src/main/java/TownBuilder/ResourceEnum.java
+++ src/main/java/TownBuilder/ResourceEnum.java
@@ -47,62 +47,6 @@ public enum ResourceEnum {
         return color;
     }
 
-    private static ArrayList<ResourceEnum> resourceArray = new ArrayList<>(Arrays.asList(WOOD, BRICK, WHEAT, GLASS, STONE));
-    public static void resetResourceArray() {
-        resourceArray = new ArrayList<>(Arrays.asList(WOOD, BRICK, WHEAT, GLASS, STONE));
-    }
-    public static ResourceEnum randomResource() {
-        ResourceEnum result;
-        try {
-            int random = (int) (Math.random() * resourceArray.size());
-            result = resourceArray.get(random);
-            resourceArray.remove(random);
-        }
-        catch (IndexOutOfBoundsException e) {
-            e.printStackTrace();
-            result = GLASS;
-        }
-        return result;
-    }
-    public static ResourceEnum resourcePicker(ResourceEnum[] blacklistedResources, BoardUI boardUI) {
-        resourceArray = new ArrayList<>(Arrays.asList(WOOD, BRICK, WHEAT, GLASS, STONE));
-        Scanner sc = new Scanner(System.in);
-        String resourceChoice;
-        while (true) {
-            System.out.println("What resource do you want?");
-            resourceChoice = sc.nextLine().toLowerCase();
-            try {
-                for (ResourceEnum blackListedResource : blacklistedResources)
-                if (blackListedResource.toString().equalsIgnoreCase(resourceChoice)) {
-                    resourceChoice = "blacklisted";
-
-                }
-            }
-            catch (NullPointerException ignored) {}
-            switch (resourceChoice) {
-                case "wheat":
-                    return WHEAT;
-                case "glass":
-                    return GLASS;
-                case "brick":
-                    return BRICK;
-                case "stone":
-                    return STONE;
-                case "wood":
-                    return WOOD;
-                case "help":
-                    return NONE;
-                case "blacklisted":
-                    System.out.println("This resource is unavailable to pick. Please choose another resource.");
-                    break;
-                default:
-                    System.out.println("Invalid input. Please try again.");
-                    break;
-            }
-        }
-        //return NONE;
-    }
-
 
 
 }
