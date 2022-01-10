24b47e5e069451eb78a0dae1230e0b27ab2de4d6
Matt Marlow <thequantumblink@gmail.com>
2022-01-10 02:48:43 -0600

    driver supports GUI code now

--- src/main/java/TownBuilder/Driver.java
+++ src/main/java/TownBuilder/Driver.java
@@ -1,23 +1,41 @@
 package TownBuilder;
 
 import TownBuilder.DebugApps.DebugTools;
+
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Driver {
 
-    public static void main (String[] args) throws IOException, URISyntaxException {
+
+    public static JFrame gameFrame = new JFrame();
+
+
+    public static void main (String[] args) throws IOException, URISyntaxException, InterruptedException {
+        initFrame();
         GameInitializer gameInitializer = new GameInitializer();
+
         DebugTools.initFile();
         gameInitializer.buildingSelection();
-        PlayerManager playerManager = new PlayerManager(gameInitializer.getBuildingsForGame());
+        PlayerManager playerManager = new PlayerManager(gameInitializer.getBuildingsForGame(), gameInitializer.getInitializationUI());
         playerManager.determineNumberOfBoards();
-        Manual.tutorial();
-        playerManager.getBoards().get(0).getManual().displayBuildings();
+//        Manual.tutorial();
+//        playerManager.getBoards().get(0).getManual().displayBuildings();
         do {
             playerManager.manageTurn();
         }
         while(playerManager.gameActive());
         System.out.println("All players have finished TownBuilder. Thanks for playing! -Matt");
+        gameFrame.setVisible(false);
+    }
+
+
+    public static JFrame getGameFrame() {
+        return gameFrame;
+    }
+    public static void initFrame() {
+        gameFrame.setSize(2560, 1440);
+        gameFrame.setVisible(true);
     }
 }
