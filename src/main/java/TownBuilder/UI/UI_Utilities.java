7f5a43a68ff550f14a2b51f353a288d1e45ed855
Matt Marlow <thequantumblink@gmail.com>
2022-01-13 23:26:39 -0600

    new method for converting absolute 1440p pixel sizes to screen percentages
    Signed-off-by: Matt Marlow <thequantumblink@gmail.com>

--- src/main/java/TownBuilder/UI/UI_Utilities.java
+++ src/main/java/TownBuilder/UI/UI_Utilities.java
@@ -5,14 +5,28 @@ import java.awt.*;
 public class UI_Utilities {
 
     static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+    //static final int SCREEN_HEIGHT = 1440;
     static final int SCREEN_HEIGHT = SCREEN_SIZE.height;
+
+    //static final int SCREEN_WIDTH = 2560;
     static final int SCREEN_WIDTH = SCREEN_SIZE.width;
+
+
     static final Dimension BUTTON_SIZE = new Dimension(50, 50);
-    static final int INTERACTIVE_PANEL_WIDTH = (int) (SCREEN_WIDTH * (420.0/ SCREEN_WIDTH));
+    static final String INTERACTIVE_PANEL_WIDTH = convertIntToPercentString(420, SCREEN_WIDTH, true);
 
 
-    public static String convertIntToPercentString(int value, int constant) {
-        System.out.println((((float) value / constant) * 100) + "sp");
-        return (((float) value / constant) * 100) + "sp";
+    public static String convertIntToPercentString(int value, int constant, boolean isWidth) {
+        String string = (((float) convertBaseValueToScaledValue(value, isWidth)  / constant) * 100) + "sp";
+        System.out.println(string);
+        return string;
+    }
+    public static int convertBaseValueToScaledValue(int value, boolean isWidth) {
+        if (isWidth) {
+            return (value * SCREEN_WIDTH) / 2560;
+        }
+        else {
+            return (value * SCREEN_HEIGHT) / 1440;
+        }
     }
 }
