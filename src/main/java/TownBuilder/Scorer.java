0a6c44915117bc479ae348a0ae5b86f163e2bfe9
Matt Marlow <thequantumblink@gmail.com>
2022-02-11 01:21:44 -0600

    clarifying comments added for Scorer describing usage of HashMap and specific booleans
    Signed-off-by: Matt Marlow <thequantumblink@gmail.com>

--- src/main/java/TownBuilder/Scorer.java
+++ src/main/java/TownBuilder/Scorer.java
@@ -9,20 +9,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Scorer {
-    public int getPreviousScore() {
-        return previousScore;
-    }
 
-    private int previousScore;
     private final Board board;
     private final ArrayList<Building> buildingsForGame;
-
     public int getResourcePenalty() {
         return resourcePenalty;
     }
-
     private int resourcePenalty;
-
     public HashMap<BuildingEnum, Integer> getScores() {
         return scores;
     }
@@ -38,8 +31,12 @@ public class Scorer {
         int totalScore = 0;
         int score;
         resourcePenalty =0;
+        /*
+            We need to check for the Caterina monument on the board, because that monument changes the scoring conditions.
+            With that monument on a Board, resources are now worth 0 instead of -1.
+         */
         boolean caterina = false;
-        scores = new HashMap<>(buildingsForGame.size());
+        scores = new HashMap<>(buildingsForGame.size()); // used to itemize scores from each color type
         DebugTools.logging("Scoring check: buildingsForGame size: " + buildingsForGame.size());
         for (Building value : buildingsForGame) {
             scores.put(value.getType(), 0);
@@ -62,6 +59,7 @@ public class Scorer {
                 }
             }
         }
+        // subtract however many resources we found (if Caterina was found, this just is 0)
         totalScore -= resourcePenalty;
 
 
